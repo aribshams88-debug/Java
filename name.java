26804bba616c86466a3bcc5f61f0625e9374d43e
aribshams88-debug <aribshams88@gmail.com>
2026-03-01 11:38:48 +0530

    Create name.java

---
+++ name.java
@@ -0,0 +1,15 @@
+class name
+{
+    String n;
+    void display()
+    {
+        System.out.println("My name is "+n);
+
+    }
+    public static void main(String arg[])
+    {
+        name ob = new name();
+        ob.n="Arib Shams";
+        ob.display();
+    }
+}
